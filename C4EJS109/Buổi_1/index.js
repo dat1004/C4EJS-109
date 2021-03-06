8be7dc2bf113807ac2b635f394c70f4cca222bdd
dat1004 <79643096+dat1004@users.noreply.github.com>
2021-03-06 12:46:25 +0700

    Delete C4EJS109 directory

--- C4EJS109/Buổi_1/index.js
+++
@@ -1,4 +0,0 @@
-alert("chào mừng bạn đến với website")
-let yob = prompt('ban sinh nam bao nhieu?')
-let age = 2021 - Number(yob)
-alert (`xin chao, nam nay ban ${age} tuoi, ban sinh nam ${yob}`)
--- C4EJS109/Buổi_2/baitap1.js
+++
@@ -1,4 +0,0 @@
-// Cho dãy số từ 50 --> 100, In ra số lẻ
-for (i = 51; i < 100; i+=2){
-    console.log(i);
-}
